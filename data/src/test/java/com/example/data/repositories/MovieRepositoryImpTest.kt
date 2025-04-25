9721e985fe2b588d7f0a97e7993ee2010edff913
yousef <yousefshaaban0@gmail.com>
2025-04-26 01:33:29 +0300

    handle unit test for movie detail repo

--- data/src/test/java/com/example/data/repositories/MovieRepositoryImpTest.kt
+++ data/src/test/java/com/example/data/repositories/MovieRepositoryImpTest.kt
@@ -5,10 +5,12 @@ import com.example.data.local.model.MovieEntity
 import com.example.data.local.model.PageDataEntity
 import com.example.data.local.model.PageWithMovies
 import com.example.data.model.movie_list.MovieResponse
+import com.example.data.model.movie_list.Result
 import com.example.data.remote.MovieAPI
 import com.example.data.util.MainCoroutineRuleTest
 import com.example.data.util.TestUtil
 import com.example.domain.entity.QueryCharacters
+import com.example.domain.entity.movie.Movie
 import com.example.domain.entity.movie.PageData
 import com.example.domain.mapper.DataMapper
 import com.example.domain.util.BatteryChecker
@@ -45,6 +47,8 @@ class MovieRepositoryImpTest {
     private val movieDao: MovieDao = mockk()
     private val dataMapperLocal: DataMapper<MovieResponse, List<MovieEntity>> = mockk()
     private val dataMapper: DataMapper<PageWithMovies, PageData> = mockk()
+    private val dataMapperMovieLocal: DataMapper<MovieEntity, Movie> = mockk()
+    private val dataMapperMovieRemote: DataMapper<Result, Movie> = mockk()
 
     private lateinit var repository: MovieRepositoryImp
 
@@ -61,7 +65,9 @@ class MovieRepositoryImpTest {
             batteryChecker,
             movieDao,
             dataMapperLocal,
-            dataMapper
+            dataMapper,
+            dataMapperMovieLocal,
+            dataMapperMovieRemote
         )
     }
 
@@ -136,4 +142,38 @@ class MovieRepositoryImpTest {
         assert(result is Resource.Error)
         coVerify { movieAPI.getMoviesNowPlayingList(queryCharacters.toQueryMap()) }
     }
+
+
+    @Test
+    fun `getMovieDetail should return cached data when available`() = runTest {
+        // Given
+
+        val cachedMovieEntity = mockk<MovieEntity>()
+        val expectedMovie = mockk<Movie>()
+
+        coEvery { movieDao.getMovieById(any()) } returns cachedMovieEntity
+        coEvery { dataMapperMovieLocal.execute(cachedMovieEntity) } returns expectedMovie
+
+        // When
+        val result = repository.getMovieDetail(1).first()
+
+        // Then
+        assertEquals(Resource.Success(expectedMovie), result)
+        coVerify(exactly = 0) { movieAPI.getMovieById(any()) }
+    }
+
+
+    @Test
+    fun `getMovieDetail on API error returns error`() = runTest {
+        coEvery { movieDao.getMovieById(any()) } returns null
+        coEvery { networkChecker.isConnected() } returns true
+        coEvery { batteryChecker.isSufficient() } returns true
+        coEvery { movieAPI.getMovieById(any()) } throws RuntimeException()
+
+        val result =
+            repository.getMovieDetail(1).first()
+        assert(result is Resource.Error)
+        coVerify { movieAPI.getMovieById(1) }
+    }
+
 }
